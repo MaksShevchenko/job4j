2cb0893a1b1bb5d038146d5b3f119412f4394f83
MaksShevchenko <sheva.maks@gmail.com>
2019-03-25 18:42:53 +0300

    1.3. JavaDoc. Code convention[#122817]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,10 +1,22 @@
 package ru.job4j.calculate;
-
+/**
+* Class Calculate решение задачи части 001 урок1.
+* @author Maks Shevchenko (sheva.maks@gmail.com)
+* @since 25.03.2019
+*/
 public class Calculate {
+	/**
+	* Медот для тестирования.
+	* @param value строка для вывода в консоль
+	* @return String value.
+	*/
    public String echo(String value) {
 	return String.format("%s %s %s", value, value, value);
 }
-
+/**
+* Main.
+* @param args - args.
+*/
 public static void main (String[] args) {
 	 Calculate calc = new Calculate();
 	 System.out.println(calc.echo("ahh"));
