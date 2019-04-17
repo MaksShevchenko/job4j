d4dc25a708e7464047a857d5cbaf3e029c40374b
MaksShevchenko <sheva.maks@gmail.com>
2019-04-17 20:21:55 +0300

    4.1. Глупый бот.[#122774]

---
+++ chapter_001/src/main/java/ru/job4j/condition/DummyBot.java
@@ -0,0 +1,26 @@
+package ru.job4j.condition;
+
+/**
+ * Class DummyBot - задание 4.1. Глупый бот.[#122774]
+ * @author Maks Shevchenko (sheva.maks@gmail.com)
+ * @since 14.04.2019
+ */
+public class DummyBot {
+    /**
+     * Отвечает на вопросы.
+     * @param question Вопрос от клиента.
+     * @return Ответ.
+     */
+    public String answer(String question) {
+        String question1 = "Привет, Бот.";
+        String question2 = "Пока.";
+        String rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
+        if ("Привет, Бот.".equals(question1)) {
+            System.out.println("Привет,умник.");
+        } else if ("Пока.".equals(question2)) {
+            System.out.println("До скорой встречи.");
+        }
+        return rsl;
+    }
+}
+
---
+++ chapter_001/src/test/java/ru/job4j/condition/DummyBotTest.java
@@ -0,0 +1,39 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Class DummyBotTest - задание 4.1. Глупый бот.[#122774]
+ * @author Maks Shevchenko (sheva.maks@gmail.com)
+ * @since 13.04.2019
+ */
+public class DummyBotTest {
+    @Test
+    public void whenGreetBot() {
+        DummyBot bot = new DummyBot();
+        assertThat(
+                bot.answer("Привет, Бот."),
+                is("Привет, умник.")
+        );
+    }
+
+    @Test
+    public void whenByeBot() {
+        DummyBot bot = new DummyBot();
+        assertThat(
+                bot.answer("Пока."),
+                is("До скорой встречи.")
+        );
+    }
+
+    @Test
+    public void whenUnknownBot() {
+        DummyBot bot = new DummyBot();
+        assertThat(
+                bot.answer("Сколько будет 2 + 2?"),
+                is("Это ставит меня в тупик. Задайте другой вопрос.")
+        );
+    }
+}
