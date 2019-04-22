da806eafc651c8bf731ba6257968135639e8bc85
MaksShevchenko <sheva.maks@gmail.com>
2019-04-22 12:43:26 +0300

    4.4. Максимум из трех чисел[#122778] исправлено

--- chapter_001/src/main/java/ru/job4j/condition/Triangle.java
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -54,7 +54,7 @@ public class Triangle {
      */
     private boolean exist(double a, double c, double b) {
         //треугольник существует когда сумма любых двух его сторон больше третьей стороны.
-        if   ( a+b <= c ||  a+c <= b ||  b+c <= a ){
+        if   ( a + b > c && a + c > b && c + b > a){
             System.out.println("Треугольник можно построить.");
         }
            return false;
