0cc1bb396d09f58e86f1b2e477118b9862ac6402
MaksShevchenko <sheva.maks@gmail.com>
2019-05-15 21:58:18 +0300

    Построить шахматную доску в псевдографике.[#122789]

--- chapter_001/src/main/java/ru/job4j/loop/Board.java
+++ chapter_001/src/main/java/ru/job4j/loop/Board.java
@@ -1,5 +1,4 @@
 package ru.job4j.loop;
-
 /**
  * Class Board решение задачи 5.3. Построить шахматную доску в псевдографике.[#122789].
  * @author Maks Shevchenko (sheva.maks@gmail.com)
@@ -14,30 +13,16 @@ public class Board {
     public String paint(int width, int height) {
         StringBuilder screen = new StringBuilder();
         String ln = System.lineSeparator();
-        /**Цикл for.
-         * Цикл для создания шахматной доски.
-         */
-        for (int i = 1; i < height; i++) {
-            for (int j = 1; j < width; j++) {
-                /**Условный оператор if.
-                 * Условие для чередования символа "X" c " ".
-                 */
-                if ((i + j) % 2 == 0) {
+        for (int x = 0; x < height; x++) {
+            for (int y = 0; y < width; y++) {
+                if ((x + y) % 2 == 0) {
                     screen.append("X");
                 } else {
                     screen.append(" ");
                 }
             }
-            /** Метод screen.append.
-             * Добавляет перевод на новую строку.
-             */
             screen.append(ln);
         }
         return screen.toString();
     }
 }
-
-/*
-    Более менее разобрался с циклами и условием для чередования.
-    Проблема StringBuilder - в курсе более детально еще рассматривается ? или же это освоить самостоятельно ?
- */
